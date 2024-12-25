5408a4e7ccfa2f2d9b40cdc12310ba70fb118bb1
Aakash Sakam <aakash@developar.in>
2024-12-25 15:32:00 +0530

    Developed mobile header and utilized scss as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./comps/header"
+import './assets/style/App.scss'
 
 function App() {
   return (
--- src/comps/header.jsx
+++ src/comps/header.jsx
@@ -5,6 +5,7 @@ import Loader from "./loading";
 
 const Header = () => {
     const [headerData, setHeaderData] = useState(null);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         fetch("/json/comps.json")
@@ -24,19 +25,38 @@ const Header = () => {
     const { logo, menu, cta } = headerData;
 
     return (
-        <header className="header py-6 border-b border-black border-opacity-10">
-            <div className="header-inner-wrap container mx-auto flex flex-wrap items-center justify-between gap-10">
-                {/* Logo */}
-                <div className="logo">
-                    <img className="max-w-52" src={logo.image.src} alt={logo.image.alt} />
+        <header className="header max-h-20 md:max-h-[86px] py-6 border-b border-black border-opacity-10 px-4">
+            <div className="header-inner-wrap md:container mx-auto flex flex-wrap items-center justify-between gap-8 lg:gap-10">
+                <div className="top-nav w-full md:w-auto flex justify-between items-center">
+                    {/* Logo */}
+                    <div className="logo">
+                        <img className="max-w-44 lg:max-w-52" src={logo.image.src} alt={logo.image.alt} />
+                    </div>
+
+                    <a
+                        onClick={() => setMenuOpen(!menuOpen)}
+                        className="menu-toggler flex flex-col gap-1.5 w-6 h-6 md:hidden focus:outline-none"
+                    >
+                        <span
+                            className={`block h-0.5 w-full bg-foreground transition-transform ${menuOpen ? "rotate-45 translate-y-2" : ""}`}
+                        ></span>
+                        <span
+                            className={`block h-0.5 w-full bg-foreground transition-opacity ${menuOpen ? "opacity-0" : ""}`}
+                        ></span>
+                        <span
+                            className={`block h-0.5 w-full bg-foreground transition-transform ${menuOpen ? "-rotate-45 -translate-y-2" : ""}`}
+                        ></span>
+                    </a>
                 </div>
 
                 {/* Navigation Menu */}
-                <nav className="menu">
-                    <ul className="menu-list flex flex-wrap items-center gap-10">
+                <nav className={`menu bg-background duration-300 ${menuOpen ? "open-menu" : ""}`}>
+                    <ul className="menu-list flex flex-wrap items-center flex-col md:flex-row gap-8 lg:gap-10">
                         {menu.map((item, index) => (
-                            <li key={index} className="menu-item text-foreground">
-                                <a href={item.link} className="group menu-link uppercase pr-6 relative h-5 opacity-40 hover:opacity-100">
+                            <li key={index} className="menu-item text-foreground w-full md:w-auto">
+                                <a
+                                    onClick={() => setMenuOpen(!menuOpen)}
+                                    href={item.link} className="group block menu-link w-full md:w-auto uppercase pr-6 relative h-5 opacity-40 hover:opacity-100">
                                     {item.title}
 
                                     <img className="w-5 h-5 absolute right-0 top-0 bottom-0 invisible group-hover:visible group-hover:rotate-90 duration-300" src={arrowTopRightRed} alt="Arrow Icon" />
@@ -45,8 +65,10 @@ const Header = () => {
                             </li>
                         ))}
 
-                        <div className="cta flex flex-wrap items-center gap-10">
-                            <a href={cta.button.link} className="brucira-btn text-sm px-4 py-2.5">
+                        <div className="cta flex flex-wrap items-center gap-8 lg:gap-10">
+                            <a
+                                onClick={() => setMenuOpen(!menuOpen)}
+                                href={cta.button.link} className="brucira-btn text-sm px-4 py-2.5">
                                 {cta.button.text}
                             </a>
                         </div>
